Add code for obtaining posterior probability distribution for the models tested. Fix normalization in dynamic programming algorithm. Improve data in CSV files by discarding middle fixation in beginning of each trial. Fix item value transformation. Add plotting of probabilities of crossing barriers and of being at each state during the dybamic programming algorithm.

dyn_prog_demo.py
@@ -23,12 +23,12 @@ initialBarrierDown = -1
 decay = 0  # decay = 0 means barriers are constant.
 barrierUp = initialBarrierUp * np.ones(maxTime)
 barrierDown = initialBarrierDown * np.ones(maxTime)
-for t in xrange(0,maxTime):
+for t in xrange(1,maxTime):
     barrierUp[t] = initialBarrierUp / (1+decay*(t+1))
     barrierDown[t] = initialBarrierDown / (1+decay*(t+1))
 
 # The vertical axis is divided into states.
-states = np.arange(initialBarrierDown, initialBarrierUp+stateStep, stateStep)
+states = np.arange(initialBarrierDown, initialBarrierUp + stateStep, stateStep)
 idx = np.where(np.logical_and(states<0.01, states>-0.01))[0]
 states[idx] = 0
 
@@ -41,8 +41,7 @@ prStates[idx] = 1
 probUpCrossing = np.zeros(maxTime)
 probDownCrossing = np.zeros(maxTime)
 
-for t in xrange(0,maxTime):
-    #print 'Time: ' + str(t)
+for t in xrange(1, maxTime):
     prStatesNew = np.zeros(states.size)
     
     # Update the probability of the states that remain inside the barriers.
@@ -50,9 +49,12 @@ for t in xrange(0,maxTime):
         currState = states[s]
         if currState > barrierDown[t] and currState < barrierUp[t]:
             change = (currState * np.ones(states.size)) - states
-            # The probability of being in state B is the sum, over all
-            # states A, of the probability of being in A at the previous
-           	# timestep times the probability of changing from A to B.
+            # The probability of being in state B is the sum, over all states A,
+            # of the probability of being in A at the previous time step times
+           	# the probability of changing from A to B. We multiply the
+            # probability by the stateStep to ensure that the area under the
+            # curve for the probability distributions probUpCrossing and
+            # probDownCrossing each add up to 1.
             prStatesNew[s] = (stateStep *
             	np.sum(np.multiply(prStates, norm.pdf(change,mean,std))))
 
@@ -80,6 +82,10 @@ for t in xrange(0,maxTime):
     probUpCrossing[t] = tempUpCross
     probDownCrossing[t] = tempDownCross
 
+    # Probabilities at each time step DO NOT add up to 1.
+    print str(np.sum(prStates) + probUpCrossing[t] + probDownCrossing[t])
+
+# Probabilities of crossing the two barriers over time add up to 1.
 print str(np.sum(probUpCrossing))
 print str(np.sum(probDownCrossing))
 print str(np.sum(probUpCrossing) + np.sum(probDownCrossing))

dyn_prog_fixations.py
@@ -16,6 +16,7 @@ import pandas as pd
 def load_data_from_csv():
     # Load experimental data from CSV file.
     # Format: parcode, trial, rt, choice, dist_left, dist_right.
+    # Angular distances to target are transformed to values in [-3, -1, 1, 3].
     df = pd.DataFrame.from_csv('expdata.csv', header=0, sep=',', index_col=None)
     subjects = df.parcode.unique()
 
@@ -38,10 +39,8 @@ def load_data_from_csv():
                 'dist_right']])
             rt[subject][trial] = dataTrial[0,0]
             choice[subject][trial] = dataTrial[0,1]
-            valueLeft[subject][trial] = np.absolute(
-                (np.absolute(dataTrial[0,2])-15)/5)
-            valueRight[subject][trial] = np.absolute(
-                (np.absolute(dataTrial[0,3])-15)/5)
+            valueLeft[subject][trial] = (-np.absolute(dataTrial[0,2])/2.5) + 3
+            valueRight[subject][trial] = (-np.absolute(dataTrial[0,3])/2.5) + 3
 
     # Load fixation data from CSV file.
     # Format: parcode, trial, fix_item, fix_time.
@@ -71,10 +70,10 @@ def load_data_from_csv():
 
 @jit("(f8,f8,f8,f8,f8[:],f8[:],f8,f8,f8)")
 def analysis_per_trial(rt, choice, valueLeft, valueRight, fixItem, fixTime, d,
-    theta, mu):
+    theta, mu, plotResults=False):
     # Parameters of the grid.
     stateStep = 0.1
-    timeStep = 1
+    timeStep = 10
     initialBarrierUp = 1
     initialBarrierDown = -1
     std = mu * d
@@ -88,12 +87,14 @@ def analysis_per_trial(rt, choice, valueLeft, valueRight, fixItem, fixTime, d,
         else:
             transitionTime += fTime // timeStep
 
-    # The total time of this trial is given by the sum of all fixations
-    # in the trial.
-    maxTime = itemFixTime + transitionTime
+    # The total time of this trial is given by the sum of all fixations in the
+    # trial.
+    maxTime = int(itemFixTime + transitionTime)
+    if maxTime == 0:
+        return 0
 
     # We start couting the trial time at the end of the transition time.
-    time = transitionTime
+    time = int(transitionTime)
 
     # The values of the barriers can change over time.
     decay = 0  # decay = 0 means barriers are constant.
@@ -103,8 +104,8 @@ def analysis_per_trial(rt, choice, valueLeft, valueRight, fixItem, fixTime, d,
         barrierUp[t] = float(initialBarrierUp) / float(1+decay*(t+1))
         barrierDown[t] = float(initialBarrierDown) / float(1+decay*(t+1))
 
-    # The vertical axis is divided into states.
-    states = np.arange(initialBarrierDown + stateStep, initialBarrierUp,
+    # The vertical axis (RDV space) is divided into states.
+    states = np.arange(initialBarrierDown, initialBarrierUp + stateStep,
         stateStep)
     idx = np.where(np.logical_and(states<0.01, states>-0.01))[0]
     states[idx] = 0
@@ -119,14 +120,21 @@ def analysis_per_trial(rt, choice, valueLeft, valueRight, fixItem, fixTime, d,
     probUpCrossing = np.zeros(maxTime)
     probDownCrossing = np.zeros(maxTime)
 
+    # Create matrix of traces to keep track of the RDV position probabilities.
+    traces = np.zeros((states.size + 2, maxTime))
+    for i in xrange(int(transitionTime)):
+        traces[1:traces.shape[0]-1,i] = prStates
+        traces[0,i] = 0
+        traces[traces.shape[0]-1,i] = 0
+
     # Iterate over all fixations in this trial.
     for fItem, fTime in zip(fixItem, fixTime):
-        # We use a distribution to model changes in RDV stochastically. The mean
-        # of the distribution (the change most likely to occur) is calculated
-        # from the model parameters and from the values of the two items.
-        if fItem == 1:  # subject is looking left.
+        # We use a normal distribution to model changes in RDV stochastically.
+        # The mean of the distribution (the change most likely to occur) is
+        # calculated from the model parameters and from the item values.
+        if fItem == 1:  # Subject is looking left.
             mean = d * (valueLeft - (theta * valueRight))
-        elif fItem == 2:  # subject is looking right.
+        elif fItem == 2:  # Subject is looking right.
             mean = d * (-valueRight + (theta * valueLeft))
         else:
             continue
@@ -145,6 +153,10 @@ def analysis_per_trial(rt, choice, valueLeft, valueRight, fixItem, fixTime, d,
                     # The probability of being in state B is the sum, over all
                     # states A, of the probability of being in A at the previous
                     # timestep times the probability of changing from A to B.
+                    # We multiply the probability by the stateStep to ensure
+                    # that the area under the curve for the probability
+                    # distributions probUpCrossing and probDownCrossing each add
+                    # up to 1.
                     prStatesNew[s] = (stateStep * np.sum(np.multiply(prStates,
                         norm.pdf(change,mean,std))))
 
@@ -172,31 +184,56 @@ def analysis_per_trial(rt, choice, valueLeft, valueRight, fixItem, fixTime, d,
             probUpCrossing[time] = tempUpCross
             probDownCrossing[time] = tempDownCross
 
+            # Update traces matrix.
+            traces[1:traces.shape[0]-1,time] = prStates
+            traces[0,time] = tempUpCross
+            traces[traces.shape[0]-1,time] = tempDownCross
+
             time += 1
 
     # Compute the log likelihood contribution of this trial based on the final
     # choice.
     likelihood = 0
-    if choice == -1:  # choice was left.
+    if choice == -1:  # Choice was left.
         if probUpCrossing[-1] > 0:
-            likelihood = np.log(probUpCrossing[-1])
-    elif choice == 1:  # choice was right.
+            likelihood = probUpCrossing[-1]
+    elif choice == 1:  # Choice was right.
         if probDownCrossing[-1] > 0:
-            likelihood = np.log(probDownCrossing[-1])
+            likelihood = probDownCrossing[-1]
+
+    if plotResults:
+        fig1 = plt.figure()
+        xAxis = np.arange(0, maxTime * timeStep, timeStep)
+        yAxis = np.arange(initialBarrierDown, initialBarrierUp + stateStep,
+            stateStep)
+        heatmap = plt.pcolor(xAxis, yAxis, np.flipud(traces))
+        plt.xlim(0, maxTime * timeStep - timeStep)
+        plt.xlabel('Time')
+        plt.ylabel('RDV')
+        plt.colorbar(heatmap)
+
+        fig2 = plt.figure()
+        plt.plot(range(len(probUpCrossing)), probUpCrossing, label='Up')
+        plt.plot(range(len(probDownCrossing)), probDownCrossing, label='Down')
+        plt.xlabel('Time')
+        plt.ylabel('P(crossing)')
+        plt.legend()
+        plt.show()
 
     return likelihood
 
 
 def run_analysis(rt, choice, valueLeft, valueRight, fixItem, fixTime, d, theta,
-    mu, useOddTrials=True, useEvenTrials=True):
+    mu, useOddTrials=True, useEvenTrials=True, verbose=True):
     likelihood = 0
     subjects = rt.keys()
     for subject in subjects:
-        print("Running subject " + subject + "...")
+        if verbose:
+            print("Running subject " + subject + "...")
         trials = rt[subject].keys()
         for trial in trials:
-            if trial % 200 == 0:
-                print("Trial " + str(trial))
+            if verbose and trial % 200 == 0:
+                print("Running trial " + str(trial) + "...")
             if not useOddTrials and trial % 2 != 0:
                 continue
             if not useEvenTrials and trial % 2 == 0:
@@ -204,9 +241,11 @@ def run_analysis(rt, choice, valueLeft, valueRight, fixItem, fixTime, d, theta,
             likelihood += analysis_per_trial(rt[subject][trial],
                 choice[subject][trial], valueLeft[subject][trial],
                 valueRight[subject][trial], fixItem[subject][trial],
-                fixTime[subject][trial], d, theta, mu)
+                fixTime[subject][trial], d, theta, mu, plotResults=False)
 
-    print("Likelihood: " + str(likelihood))
+    if verbose:
+        print("Likelihood for " + str(d) + ", " + str(theta) + ", " + str(mu) +
+            ": " + str(likelihood))
     return likelihood
 
 
@@ -215,7 +254,7 @@ def run_analysis_wrapper(params):
 
 
 def main():
-    numThreads = 4
+    numThreads = 8
     pool = Pool(numThreads)
 
     data = load_data_from_csv()

dyn_prog_group_fitting.py
@@ -24,11 +24,11 @@ def generate_choice_curve_for_data(choice, valueLeft, valueRight):
         trials = choice[subject].keys()
         for trial in trials:
             valueDiff = valueLeft[subject][trial] - valueRight[subject][trial]
-            idx = valueDiff + 3
-            if choice[subject][trial] == -1:  # choice was left
+            idx = (valueDiff / 2) + 3
+            if choice[subject][trial] == -1:  # Choice was left.
                 countLeftChosen[idx] +=1
                 countTotal[idx] += 1
-            elif choice[subject][trial] == 1:  # choice was right
+            elif choice[subject][trial] == 1:  # Choice was right.
                 countTotal[idx] += 1
 
     stdProbLeftChosen = np.zeros(7)
@@ -39,7 +39,7 @@ def generate_choice_curve_for_data(choice, valueLeft, valueRight):
             (1 - probLeftChosen[i])) / countTotal[i])
 
     fig = plt.figure()
-    plt.errorbar(range(-3,4,1), probLeftChosen, yerr=stdProbLeftChosen,
+    plt.errorbar(range(-6,8,2), probLeftChosen, yerr=stdProbLeftChosen,
         label='Actual data')
     plt.xlabel('Value difference')
     plt.ylabel('P(choose left)')
@@ -54,11 +54,11 @@ def generate_choice_curve_for_simulations(choice, valueLeft, valueRight,
 
     for trial in xrange(0, numTrials):
         valueDiff = valueLeft[trial] - valueRight[trial]
-        idx = valueDiff + 3
-        if choice[trial] == -1:  # choice was left
+        idx = (valueDiff / 2) + 3
+        if choice[trial] == -1:  # Choice was left.
             countLeftChosen[idx] +=1
             countTotal[idx] += 1
-        elif choice[trial] == 1:  # choice was right
+        elif choice[trial] == 1:  # Choice was right.
             countTotal[idx] += 1
 
     stdProbLeftChosen = np.zeros(7)
@@ -69,7 +69,7 @@ def generate_choice_curve_for_simulations(choice, valueLeft, valueRight,
             (1 - probLeftChosen[i])) / countTotal[i])
 
     fig = plt.figure()
-    plt.errorbar(range(-3,4,1), probLeftChosen, yerr=stdProbLeftChosen,
+    plt.errorbar(range(-6,8,2), probLeftChosen, yerr=stdProbLeftChosen,
         label='Simulations')
     plt.xlabel('Value difference')
     plt.ylabel('P(choose left)')
@@ -79,7 +79,7 @@ def generate_choice_curve_for_simulations(choice, valueLeft, valueRight,
 
 def generate_rt_dist_for_data(rt, valueLeft, valueRight):
     rtsPerValueDiff = dict()
-    for valueDiff in xrange(-3,4,1):
+    for valueDiff in xrange(-6,8,2):
         rtsPerValueDiff[valueDiff] = list()
 
     subjects = rt.keys()
@@ -91,14 +91,14 @@ def generate_rt_dist_for_data(rt, valueLeft, valueRight):
 
     meanRts = np.zeros(7)
     stdRts = np.zeros(7)
-    for valueDiff in xrange(-3,4,1):
-        idx = valueDiff + 3
+    for valueDiff in xrange(-6,8,2):
+        idx = (valueDiff / 2) + 3
         meanRts[idx] = np.mean(np.array(rtsPerValueDiff[valueDiff]))
         stdRts[idx] = (np.std(np.array(rtsPerValueDiff[valueDiff])) /
             np.sqrt(len(rtsPerValueDiff[valueDiff])))
 
     fig = plt.figure()
-    plt.errorbar(range(-3,4,1), meanRts, yerr=stdRts, label='Actual data')
+    plt.errorbar(range(-6,8,2), meanRts, yerr=stdRts, label='Actual data')
     plt.xlabel('Value difference')
     plt.ylabel('Mean RT')
     plt.legend()
@@ -107,7 +107,7 @@ def generate_rt_dist_for_data(rt, valueLeft, valueRight):
 
 def generate_rt_dist_for_simulations(rt, valueLeft, valueRight, numTrials):
     rtsPerValueDiff = dict()
-    for valueDiff in xrange(-3,4,1):
+    for valueDiff in xrange(-6,8,2):
         rtsPerValueDiff[valueDiff] = list()
 
     for trial in xrange(0, numTrials):
@@ -116,18 +116,19 @@ def generate_rt_dist_for_simulations(rt, valueLeft, valueRight, numTrials):
 
     meanRts = np.zeros(7)
     stdRts = np.zeros(7)
-    for valueDiff in xrange(-3,4,1):
-        idx = valueDiff + 3
+    for valueDiff in xrange(-6,8,2):
+        idx = (valueDiff / 2) + 3
         meanRts[idx] = np.mean(np.array(rtsPerValueDiff[valueDiff]))
         stdRts[idx] = (np.std(np.array(rtsPerValueDiff[valueDiff])) /
             np.sqrt(len(rtsPerValueDiff[valueDiff])))
 
     fig = plt.figure()
-    plt.errorbar(range(-3,4,1), meanRts, yerr=stdRts, label='Simulations')
+    plt.errorbar(range(-6,8,2), meanRts, yerr=stdRts, label='Simulations')
     plt.xlabel('Value difference')
     plt.ylabel('Mean RT')
     plt.legend()
     return fig
+
 
 def save_simulations_to_csv(choice, rt, valueLeft, valueRight, fixItem,
     fixTime, numTrials):
@@ -137,11 +138,11 @@ def save_simulations_to_csv(choice, rt, valueLeft, valueRight, fixItem,
 
     for trial in xrange(0, numTrials):
         valueDiff = valueLeft[trial] - valueRight[trial]
-        idx = valueDiff + 3
-        if choice[trial] == -1:  # choice was left
+        idx = (valueDiff / 2) + 3
+        if choice[trial] == -1:  # Choice was left.
             countLeftChosen[idx] +=1
             countTotal[idx] += 1
-        elif choice[trial] == 1:  # choice was right
+        elif choice[trial] == 1:  # Choice was right.
             countTotal[idx] += 1
 
     probLeftChosen = np.zeros(7)
@@ -157,7 +158,7 @@ def save_simulations_to_csv(choice, rt, valueLeft, valueRight, fixItem,
 
     # Reaction times.
     rtsPerValueDiff = dict()
-    for valueDiff in xrange(-3,4,1):
+    for valueDiff in xrange(-6,8,2):
         rtsPerValueDiff[valueDiff] = list()
 
     for trial in xrange(0, numTrials):
@@ -166,8 +167,8 @@ def save_simulations_to_csv(choice, rt, valueLeft, valueRight, fixItem,
 
     meanRts = np.zeros(7)
     stdRts = np.zeros(7)
-    for valueDiff in xrange(-3,4,1):
-        idx = valueDiff + 3
+    for valueDiff in xrange(-6,8,2):
+        idx = (valueDiff / 2) + 3
         meanRts[idx] = np.mean(np.array(rtsPerValueDiff[valueDiff]))
         stdRts[idx] = (np.std(np.array(rtsPerValueDiff[valueDiff])) /
             np.sqrt(len(rtsPerValueDiff[valueDiff])))
@@ -184,18 +185,18 @@ def save_simulations_to_csv(choice, rt, valueLeft, valueRight, fixItem,
 
     for trial in xrange(0, numTrials):
         valueDiff = valueLeft[trial] - valueRight[trial]
-        idx = valueDiff + 3
-        if fixItem[trial][1] == 1:  # first item was left
-            if choice[trial] == -1:  # choice was left
+        idx = (valueDiff / 2) + 3
+        if fixItem[trial][1] == 1:  # First item was left.
+            if choice[trial] == -1:  # Choice was left.
                 countLeftChosenLeft[idx] +=1
                 countTotalLeft[idx] += 1
-            elif choice[trial] == 1:  # choice was right
+            elif choice[trial] == 1:  # Choice was right.
                 countTotalLeft[idx] += 1
-        if fixItem[trial][1] == 2:  # first item was right
-            if choice[trial] == -1:  # choice was left
+        if fixItem[trial][1] == 2:  # First item was right.
+            if choice[trial] == -1:  # Choice was left.
                 countLeftChosenRight[idx] +=1
                 countTotalRight[idx] += 1
-            elif choice[trial] == 1:  # choice was right
+            elif choice[trial] == 1:  # Choice was right.
                 countTotalRight[idx] += 1
 
     probLeftChosenLeft = np.zeros(7)
@@ -223,18 +224,18 @@ def save_simulations_to_csv(choice, rt, valueLeft, valueRight, fixItem,
 
     for trial in xrange(0, numTrials):
         valueDiff = valueLeft[trial] - valueRight[trial]
-        idx = valueDiff + 3
-        if fixItem[trial][-1] == 1:  # last item was left
-            if choice[trial] == -1:  # choice was left
+        idx = (valueDiff / 2) + 3
+        if fixItem[trial][-1] == 1:  # Last item was left.
+            if choice[trial] == -1:  # Choice was left.
                 countLeftChosenLeft[idx] +=1
                 countTotalLeft[idx] += 1
-            elif choice[trial] == 1:  # choice was right
+            elif choice[trial] == 1:  # Choice was right.
                 countTotalLeft[idx] += 1
-        if fixItem[trial][-1] == 2:  # last item was right
-            if choice[trial] == -1:  # choice was left
+        if fixItem[trial][-1] == 2:  # Last item was right.
+            if choice[trial] == -1:  # Choice was left.
                 countLeftChosenRight[idx] +=1
                 countTotalRight[idx] += 1
-            elif choice[trial] == 1:  # choice was right
+            elif choice[trial] == 1:  # Choice was right.
                 countTotalRight[idx] += 1
 
     probLeftChosenLeft = np.zeros(7)
@@ -262,7 +263,7 @@ def save_simulations_to_csv(choice, rt, valueLeft, valueRight, fixItem,
 
     for trial in xrange(0, numTrials):
         valueDiff = valueLeft[trial] - valueRight[trial]
-        idx = valueDiff + 3
+        idx = (valueDiff / 2) + 3
 
         # Get total fixation time for each item.
         fixTimeLeft = 0
@@ -273,17 +274,17 @@ def save_simulations_to_csv(choice, rt, valueLeft, valueRight, fixItem,
             elif fixItem[trial][i] == 2:
                 fixTimeRight += fixTime[trial][i]
 
-        if fixTimeLeft >= fixTimeRight:  # longest fixated item was left
-            if choice[trial] == -1:  # choice was left
+        if fixTimeLeft >= fixTimeRight:  # Longest fixated item was left.
+            if choice[trial] == -1:  # Choice was left.
                 countLeftChosenLeft[idx] +=1
                 countTotalLeft[idx] += 1
-            elif choice[trial] == 1:  # choice was right
+            elif choice[trial] == 1:  # Choice was right.
                 countTotalLeft[idx] += 1
-        else:  # longest fixated item was right
-            if choice[trial] == -1:  # choice was left
+        else:  # Longest fixated item was right.
+            if choice[trial] == -1:  # Choice was left.
                 countLeftChosenRight[idx] +=1
                 countTotalRight[idx] += 1
-            elif choice[trial] == 1:  # choice was right
+            elif choice[trial] == 1:  # Choice was right.
                 countTotalRight[idx] += 1
 
     probLeftChosenLeft = np.zeros(7)
@@ -322,13 +323,20 @@ def main():
     print("Starting coarse grid search...")
     rangeD = [0.0002, 0.0003, 0.0004]
     rangeTheta = [0.3, 0.5, 0.7]
-    rangeMu = [80, 100, 120]
+    rangeMu = [400, 500, 600]
 
     models = list()
     list_params = list()
+    modelLikelihoods = dict()
+    modelLikelihoods['d'] = list()
+    modelLikelihoods['theta'] = list()
+    modelLikelihoods['mu'] = list()
     for d in rangeD:
         for theta in rangeTheta:
             for mu in rangeMu:
+                modelLikelihoods['d'].append(d)
+                modelLikelihoods['theta'].append(theta)
+                modelLikelihoods['mu'].append(mu)
                 models.append((d, theta, mu))
                 params = (rt, choice, valueLeft, valueRight, fixItem, fixTime,
                     d, theta, mu, True, False)
@@ -347,6 +355,11 @@ def main():
     print("Optimal theta: " + str(optimTheta))
     print("Optimal mu: " + str(optimMu))
 
+    # Save coarse grid search results to CSV file.
+    modelLikelihoods['L'] = results_coarse
+    df = pd.DataFrame(modelLikelihoods)
+    df.to_csv('likelihood_coarse.csv', header=0, sep=',', index_col=None)
+
     # Fine grid search on the parameters of the model.
     print("Starting fine grid search...")
     rangeD = [optimD-0.000025, optimD, optimD+0.000025]
@@ -355,9 +368,16 @@ def main():
 
     models = list()
     list_params = list()
+    modelLikelihoods = dict()
+    modelLikelihoods['d'] = list()
+    modelLikelihoods['theta'] = list()
+    modelLikelihoods['mu'] = list()
     for d in rangeD:
         for theta in rangeTheta:
             for mu in rangeMu:
+                modelLikelihoods['d'].append(d)
+                modelLikelihoods['theta'].append(theta)
+                modelLikelihoods['mu'].append(mu)
                 models.append((d, theta, mu))
                 params = (rt, choice, valueLeft, valueRight, fixItem, fixTime,
                     d, theta, mu, True, False)
@@ -375,6 +395,11 @@ def main():
     print("Optimal d: " + str(optimD))
     print("Optimal theta: " + str(optimTheta))
     print("Optimal mu: " + str(optimMu))
+
+    # Save fine grid search results to CSV file.
+    modelLikelihoods['L'] = results_fine
+    df = pd.DataFrame(modelLikelihoods)
+    df.to_csv('likelihood_fine.csv', header=0, sep=',', index_col=None)
 
     # Get empirical distributions from even trials.
     even_dists = get_empirical_distributions(rt, choice, valueLeft, valueRight,

dyn_prog_test_algo.py
@@ -14,7 +14,7 @@ from dyn_prog_fixations import load_data_from_csv, analysis_per_trial
 
 def get_empirical_distributions(rt, choice, valueLeft, valueRight, fixItem,
     fixTime, useOddTrials=True, useEvenTrials=True):
-    valueDiffs = xrange(0,4,1)
+    valueDiffs = range(-6,8,2)
 
     countLeftFirst = 0
     countTotalTrials = 0
@@ -33,9 +33,10 @@ def get_empirical_distributions(rt, choice, valueLeft, valueRight, fixItem,
             if not useEvenTrials and trial % 2 == 0:
                 continue
 
-            # Get value difference between best and worst values for this trial.
-            valueDiff = np.absolute(valueLeft[subject][trial] -
-                valueRight[subject][trial])
+            # Get value difference between best and worst items for this trial.
+            valueDiff = (max(valueLeft[subject][trial],
+                valueRight[subject][trial]) - min(valueLeft[subject][trial],
+                valueRight[subject][trial]))
             # Iterate over this trial's fixations.
             firstFix = True
             transitionTime = 0
@@ -48,7 +49,7 @@ def get_empirical_distributions(rt, choice, valueLeft, valueRight, fixItem,
                         firstFix = False
                         distFirstFixList.append(fixTime[subject][trial][i])
                         countTotalTrials +=1
-                        if item == 1:  # first fixation was left
+                        if item == 1:  # First fixation was left.
                             countLeftFirst +=1
                     else:
                         distMiddleFixList[valueDiff].append(
@@ -70,7 +71,7 @@ def get_empirical_distributions(rt, choice, valueLeft, valueRight, fixItem,
 
 def run_simulations(numTrials, trialConditions, d, theta, mu, probLeftFixFirst,
     distTransition, distFirstFix, distMiddleFix):
-    timeStep = 1
+    timeStep = 10
     L = 1
 
     # Simulation data to be returned.
@@ -84,9 +85,9 @@ def run_simulations(numTrials, trialConditions, d, theta, mu, probLeftFixFirst,
     trialCount = 0
 
     for trialCondition in trialConditions:
-        vLeft = np.absolute((np.absolute(trialCondition[0])-15)/5)
-        vRight = np.absolute((np.absolute(trialCondition[1])-15)/5)
-        valueDiff = np.absolute(vLeft - vRight)
+        vLeft = (-np.absolute(trialCondition[0]) / 2.5) + 3
+        vRight = (-np.absolute(trialCondition[1]) / 2.5) + 3
+        valueDiff = max(vLeft, vRight) - min(vLeft, vRight)
         for trial in xrange(numTrials):
             fixItem[trialCount] = list()
             fixTime[trialCount] = list()
@@ -112,9 +113,9 @@ def run_simulations(numTrials, trialConditions, d, theta, mu, probLeftFixFirst,
                     # stochastically. The mean of the distribution (the change
                     # most likely to occur) is calculated from the model
                     # parameters and from the values of the two items.
-                    if currFixItem == 1:  # subject is looking left.
+                    if currFixItem == 1:  # Subject is looking left.
                         mean = d * (vLeft - (theta * vRight))
-                    elif currFixItem == 2:  # subject is looking right.
+                    elif currFixItem == 2:  # Subject is looking right.
                         mean = d * (-vRight + (theta * vLeft))
 
                     # Sample the change in RDV from the distribution.
@@ -143,7 +144,7 @@ def run_simulations(numTrials, trialConditions, d, theta, mu, probLeftFixFirst,
 
                 # Add previous fixation to this trial's data.
                 fixItem[trialCount].append(currFixItem)
-                fixTime[trialCount].append(t * timeStep)
+                fixTime[trialCount].append(currFixTime)
 
                 # Sample next fixation for this trial.
                 if currFixItem == 1:
@@ -158,13 +159,15 @@ def run_simulations(numTrials, trialConditions, d, theta, mu, probLeftFixFirst,
 
 
 def run_analysis(numTrials, rt, choice, valueLeft, valueRight, fixItem, fixTime,
-    d, theta, mu):
-    likelihood = 0
+    d, theta, mu, verbose=True):
+    logLikelihood = 0
     for trial in xrange(numTrials):
-        likelihood += analysis_per_trial(rt[trial], choice[trial],
+        if verbose and trial % 100 == 0:
+            print("Trial " + str(trial) + "/" + str(numTrials) + "...")
+        logLikelihood += np.log(analysis_per_trial(rt[trial], choice[trial],
             valueLeft[trial], valueRight[trial], fixItem[trial], fixTime[trial],
-            d, theta, mu)
-    return likelihood
+            d, theta, mu, plotResults=False))
+    return logLikelihood
 
 
 def run_analysis_wrapper(params):
@@ -172,7 +175,7 @@ def run_analysis_wrapper(params):
 
 
 def main():
-    numThreads = 4
+    numThreads = 8
     pool = Pool(numThreads)
 
     # Load experimental data from CSV file.
@@ -194,9 +197,9 @@ def main():
 
     # Parameters for fake data generation.
     numTrials = 100
-    d = 0.01
-    theta = 0.5
-    mu = 100
+    d = 0.0003
+    theta = 0.3
+    mu = 400
 
     orientations = range(-15,20,5)
     trialConditions = list()
@@ -206,6 +209,7 @@ def main():
                 trialConditions.append((oLeft, oRight))
 
     # Generate fake data.
+    print("Running simulations...")
     simul = run_simulations(numTrials, trialConditions, d, theta, mu,
         probLeftFixFirst, distTransition, distFirstFix, distMiddleFix)
     simulRt = simul.rt
@@ -216,9 +220,9 @@ def main():
     simulFixTime = simul.fixTime
 
     # Grid search to recover the parameters.
-    rangeD = [0.01, 0.02, 0.03]
-    rangeTheta = [0.5, 0.7, 0.9]
-    rangeMu = [80, 100, 120]
+    rangeD = [0.00025, 0.0003, 0.00035]
+    rangeTheta = [0.2, 0.3, 0.4]
+    rangeMu = [350, 400, 450]
 
     totalTrials = numTrials * len(trialConditions)
     models = list()
@@ -231,8 +235,6 @@ def main():
                 params = (totalTrials, simulRt, simulChoice, simulValueLeft,
                     simulValueRight, simulFixItem, simulFixTime, d, theta, mu)
                 list_params.append(params)
-                results.append(run_analysis_wrapper(params))
-
 
     print("Starting pool of workers...")
     results = pool.map(run_analysis_wrapper, list_params)